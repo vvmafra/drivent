f357f6180c747236849dd31a8b79ba925080ae16
Victor Mafra <victorvmafra97@gmail.com>
2023-07-14 17:26:09 -0300

    errors sequence errors Hotel

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -5,53 +5,50 @@ import ticketsRepository from "@/repositories/tickets-repository"
 import httpStatus from "http-status"
 
 async function getHotels(userId: number){
-    const hotels = await hotelsRepository.findHotels()
 
     const enrollment = await enrollmentRepository.findWithAddressByUserId(userId)
-
-    const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id)
-
-    console.log(hotels.length)
 
     if (!enrollment) {
         throw notFoundError()
     }
 
+    const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id)
+
     if (!ticket) {
         throw notFoundError()
     }
 
-    if (ticket.status === "RESERVED") {
+    if (ticket.status === "RESERVED" || ticket.TicketType.isRemote === true || ticket.TicketType.includesHotel === false) {
         throw requestError(httpStatus.PAYMENT_REQUIRED, "PaymentRequired")
     }
 
-    if (ticket.TicketType.isRemote === true) {
-        throw requestError(httpStatus.PAYMENT_REQUIRED, "PaymentRequired")
+    const hotels = await hotelsRepository.findHotels()
+
+    if (hotels.length === 0) {
+        throw notFoundError()
     }
 
-    if (ticket.TicketType.includesHotel === false) {
-        throw requestError(httpStatus.PAYMENT_REQUIRED, "PaymentRequired")
-    }
-
-    
 
     return hotels
 }
 
 async function getHotelId(userId: number, hotelId:number) {
-    const hotel = (await hotelsRepository.findHotelId(hotelId)).hotel
-    const hotelWithRooms = (await hotelsRepository.findHotelId(hotelId)).hotelWithRooms
-
+    
     const enrollment = await enrollmentRepository.findWithAddressByUserId(userId)
+
+    if (!enrollment)  throw notFoundError()
 
     const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id)
 
-    if (!enrollment || !ticket) throw notFoundError()
+    if (!ticket) throw notFoundError()
 
     if (ticket.status === "RESERVED" || ticket.TicketType.isRemote === true || ticket.TicketType.includesHotel === false) {
         throw requestError(httpStatus.PAYMENT_REQUIRED, "PaymentRequired")
     }
     
+    const hotel = (await hotelsRepository.findHotelId(hotelId)).hotel
+    const hotelWithRooms = (await hotelsRepository.findHotelId(hotelId)).hotelWithRooms
+
     if (!hotel) {
         throw notFoundError()
     }
