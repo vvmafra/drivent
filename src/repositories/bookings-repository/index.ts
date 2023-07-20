a9623d86467e359e94b44e16aa5f0e50182dfdaa
Victor Mafra <victorvmafra97@gmail.com>
2023-07-20 12:46:43 -0300

    minor:ObjBooking

--- src/repositories/bookings-repository/index.ts
+++ src/repositories/bookings-repository/index.ts
@@ -1,11 +1,24 @@
 import { prisma } from '@/config';
 
 async function findBookings(userId: number){
-  return prisma.booking.findFirst({
+  const booking = await prisma.booking.findFirst({
     where: {
         userId
     }
   });
+
+  const room =  await prisma.room.findFirst({
+    where: {
+      id: booking.roomId
+    }
+  })
+
+  const bookingObj = {
+    id: booking.id,
+    Room: room
+
+  }
+  return bookingObj
 }
 
 export default {
